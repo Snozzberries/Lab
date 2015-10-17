91c6aa258374e28585cb8e023ef1986539bd668b
Michael <mike@soule.biz>
2015-10-17 16:02:30 -0700

    Configuration
    Concept of single central point of configuration

---
+++ configuration.ps1
@@ -0,0 +1,17 @@
+﻿#############################
+### General Configuration ###
+#############################
+$generalProperties = @{
+    'Administrator Name' = 'Michael Soule';
+}
+
+$generalSettings = New-Object -TypeName System.Management.Automation.PSObject -Property $generalProperties
+
+####################
+### Server Roles ###
+####################
+$serverRoleProperties = @{
+
+}
+
+$serverRoleSettings = New-Object -TypeName System.Management.Automation.PSObject -Property $serverRoleProperties
